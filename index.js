7ee563ea95ea722d6d3cc7e24cc37e3b1f7f445b
InspiredGH <solomon.asante62@gmail.com>
2022-12-10 12:11:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-alert("Hello-World");
-alert(typeof(23));
-alert(typeof("Solomon"));
-alert(typeof(false));
-alert(typeof(0.5));
+//
+// function test() {
+//     var a = "3";
+//     var b = "8";
+//
+// var c = a;
+//     a= b;
+//     b= c;
+//
+//     alert("a is " + a);
+//     alert("b is " + b);
+// }
+//
+// test();
+
+
+// var name = "Solomon";
+// var length = 180;
+// var messages = prompt("Hello there, " + name + "," + " Kindly Enter your Message");
+//
+// alert("You have written " + messages.length + " characters, you have " + (length - messages.length) + " characters left.");
+
+
+var length = 180;
+var name = "Solomon";
+var messages = prompt("Hello there, " + name + "," + " Kindly Enter your Message");
+messages.slice(0,181);
+
+if (messages.length <= 180) {
+    alert("You have written " + messages.length + " characters");
+    alert(messages);
+} else {
+    alert("You have exceeded the Max length allowed." + (length - messages.length) + " Message sliced");
+    alert(messages);
+}
